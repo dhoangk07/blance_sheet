d4e7b2b7443b8348c02bba6b51bed197a0d6c951
Hoang Dinh <32945097+dhoangk07@users.noreply.github.com>
2024-05-08 22:05:35 +0700

    Refactor code

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import streamlit as st
-from group_by import group_by_keyword
+from group_by import group_by_keyword, group_by_column_type
+
+df_must_have = group_by_column_type('Must Have')
+st.write("Must Have")
+st.bar_chart(df_must_have.set_index('Time'))
+
+df_nice_to_have = group_by_column_type('Nice To Have')
+st.write("Nice To Have")
+st.bar_chart(df_nice_to_have.set_index('Time'))
+
+df_wasted = group_by_column_type('Wasted')
+st.write("Wasted")
+st.bar_chart(df_wasted.set_index('Time'))
 
 df_xang_jupiter = group_by_keyword("Xăng jupiter", 'Must Have')
 st.write("Xăng jupiter")
@@ -24,6 +36,14 @@ st.bar_chart(df_ve_so.set_index('Time'))
 df_bivina = group_by_keyword("Bivina", 'Nice To Have')
 st.write("Bivina")
 st.bar_chart(df_bivina.set_index('Time'))
+
+df_banhcan = group_by_keyword("Bánh căn", 'Must Have')
+st.write("Bánh căn")
+st.bar_chart(df_banhcan.set_index('Time'))
+
+df_cfe = group_by_keyword("Cfe", 'Nice To Have')
+st.write("Cfe")
+st.bar_chart(df_cfe.set_index('Time'))
 
 df_lotter = group_by_keyword("Lotte", 'Nice To Have')
 st.write("Lotte")
